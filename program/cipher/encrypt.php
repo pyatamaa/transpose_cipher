ec5f6b643a55f7ef58d1a201fca254ce93fabd5d
Nur Pratama <92738041+pyatamaa@users.noreply.github.com>
2023-11-10 21:58:57 +0700

    Update encrypt.php
    mengubah function yang tadinya menggunakan metode rail fence cipher menjadi transpose row column cipher

--- program/cipher/encrypt.php
+++ program/cipher/encrypt.php
@@ -1,73 +1,30 @@
-<!-- <?php
-
-//   $username = ($_POST["username"]);
-//   $password = ($_POST["password"]);
-//   $key = "tugas";
-
-//   function vigenere($src, $key, $is_encode){
-//       $key = strtoupper($key);
-//       $src = strtoupper($src);
-//       $dest = '';
-
-//       /* strip out non-letters */
-//       for($i = 0; $i <= strlen($src); $i++) {
-//           $char = substr($src, $i, 1);
-//           if(ctype_upper($char)) {
-//               $dest .= $char;
-//           }
-//       }
-
-//       for($i = 0; $i <= strlen($dest); $i++) {
-//           $char = substr($dest, $i, 1);
-//           if(!ctype_upper($char)) {
-//               continue;
-//           }
-//           $dest = substr_replace($dest,
-//               chr (
-//                   ord('A') +
-//                   ($is_encode
-//                   ? ord($char) - ord('A') + ord($key[$i % strlen($key)]) - ord('A')
-//                   : ord($char) - ord($key[$i % strlen($key)]) + 26
-//                   ) % 26
-//               )
-//           , $i, 1);
-//       }
-//       return $dest;
-//     }
-
-
-function encrypt($plainText, $key) {
-    $length = strlen($plainText);
-    $rail = array_fill(0, $key, '');
-    
+<?php
+// ini adalah function dekripsi dengan metode row column cipher
+function decrypt($cipherText, $key) {
+    $length = strlen($cipherText);
+    $plainText = array_fill(0, $length, '\0');
     $index = 0;
-    $down = false;
 
-    for ($i = 0; $i < $length; $i++) {
-        $rail[$index] .= $plainText[$i];
-
-        if ($index == 0 || $index == $key - 1) {
-            $down = !$down;
-        }
-
-        $down ? $index++ : $index--;
-    }
-
-    $result = '';
     for ($i = 0; $i < $key; $i++) {
-        $result .= $rail[$i];
+        for ($j = $i; $j < $length; $j += $key) {
+            if (ctype_alpha($cipherText[$j])) {
+                while (!ctype_alpha($plainText[$index % $length])) {
+                    $index++;
+                }
+                $plainText[$index % $length] = $cipherText[$j];
+                $index++;
+            } else {
+                $plainText[$j] = $cipherText[$j];
+            }
+        }
     }
 
-    return $result;
+    return implode('', $plainText);
 }
 
-$username = $_POST["username"];
-$password = $_POST["password"];
+$encryptedPassword = $_POST["password"];
 $key = 3; // Anda bisa mengubah ini sesuai kebutuhan
 
-$encryptedPassword = encrypt($password, $key);
-
-// Gunakan $encryptedPassword untuk sistem login Anda
-
-
+$decryptedPassword = decrypt($encryptedPassword, $key);
+// Gunakan $decryptedPassword untuk sistem login Anda
 ?>
